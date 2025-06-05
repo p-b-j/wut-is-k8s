8127ae8fc99c10d2532bfc31bf9f9a3e8cc0f6f5
Porter Jones <porter@mechanical-orchard.com>
2025-06-05 13:42:57 +0100

    Add initial flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "exploring k8s";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ ];
+        };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [ ];
+
+          };
+        };
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
